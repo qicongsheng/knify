1cb6900dcb482812343bf98f81b5955a3a26355a
csqi <congsheng.qi@freemud.cn>
2025-01-06 02:23:23 +0800

    init

--- knify/help.py
+++ knify/help.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 # Author: qicongsheng
+
 def get_pip_name():
     return 'knify'
 
 def get_version():
-    return '1.6.90'
+    return '1.6.92'
 
 def print_version():
     print('''Knify %s
