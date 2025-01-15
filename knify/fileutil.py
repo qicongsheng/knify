9dd8dba7046016745c90df3b64c1e61465054808
qicongsheng <875881559@qq.com>
2025-01-15 20:39:03 +0800

    init

--- knify/fileutil.py
+++ knify/fileutil.py
@@ -8,6 +8,6 @@ def read_to_string(file_path_: str, encoding_: str = 'utf-8') -> str:
         return file.read()
 
 
-def read_lines(file_path_: str, encoding_: str = 'utf-8', trim: bool = False) -> list[str]:
+def read_lines(file_path_: str, encoding_: str = 'utf-8', trim: bool = True) -> list[str]:
     with open(file_path_, 'r', encoding=encoding_) as file:
         return [line_.strip() for line_ in file.readlines()] if trim else file.readlines()
--- knify/help.py
+++ knify/help.py
@@ -8,7 +8,7 @@ def get_pip_name():
 
 
 def get_version():
-    return '1.8.45'
+    return '1.8.46'
 
 
 def print_version():
