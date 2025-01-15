9f10f37cfd852a26624327282370b6cda50cfa63
qicongsheng <875881559@qq.com>
2025-01-15 20:46:13 +0800

    init

--- knify/fileutil.py
+++ knify/fileutil.py
@@ -3,11 +3,11 @@
 # Author: qicongsheng
 
 
-def read_to_string(file_path_: str, encoding_: str = 'utf-8') -> str:
-    with open(file_path_, 'r', encoding=encoding_) as file:
+def read_to_string(file_path: str, encoding: str = 'utf-8') -> str:
+    with open(file_path, 'r', encoding=encoding) as file:
         return file.read()
 
 
-def read_lines(file_path_: str, encoding_: str = 'utf-8', trim: bool = True) -> list[str]:
-    with open(file_path_, 'r', encoding=encoding_) as file:
+def read_lines(file_path: str, encoding: str = 'utf-8', trim: bool = True) -> list[str]:
+    with open(file_path, 'r', encoding=encoding) as file:
         return [line_.strip() for line_ in file.readlines()] if trim else file.readlines()
--- knify/help.py
+++ knify/help.py
@@ -8,7 +8,7 @@ def get_pip_name():
 
 
 def get_version():
-    return '1.8.46'
+    return '1.8.47'
 
 
 def print_version():
