687f38ab814cc91e844a22d15502a03839660f10
qicongsheng <875881559@qq.com>
2025-02-18 02:10:39 +0800

    init

--- knify/curlutil.py
+++ knify/curlutil.py
@@ -7,14 +7,19 @@ import curlify
 import requests
 import uncurl
 
-from . import warnutil
+from knify import warnutil
 
 
 def request(curl_command: str) -> Optional[requests.Response]:
     warnutil.disable_ssl_warnings()
+    # 解析--location参数, uncurl默认不支持
+    follow_redirects = "--location" in curl_command or "-L" in curl_command
+    curl_command = curl_command.replace("--location", "").replace("-L", "")
+
     context = uncurl.parse_context(curl_command)
     method = context.method.lower() if context.method else 'get'
-    return requests.request(method, context.url, headers=context.headers, data=context.data)
+    return requests.request(method.upper(), context.url, headers=context.headers, data=context.data,
+                            allow_redirects=follow_redirects)
 
 
 def to_curl(req):
--- knify/help.py
+++ knify/help.py
@@ -6,7 +6,7 @@ def get_name():
 
 
 def get_version():
-    return '1.8.89'
+    return '1.8.90'
 
 
 def print_version():
