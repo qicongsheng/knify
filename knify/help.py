67058a86bac5eff8033683cd7979884cb96aafc8
qicongsheng <875881559@qq.com>
2025-05-20 14:23:20 +0800

    PartitionExecutor

--- knify/help.py
+++ knify/help.py
@@ -6,7 +6,7 @@ def get_name():
 
 
 def get_version():
-    return '1.9.40'
+    return '1.9.41'
 
 
 def print_version():
--- knify/threadutil.py
+++ knify/threadutil.py
@@ -9,46 +9,6 @@ from knify import dateutil
 from knify import listutil
 from knify import logger
 
-task_lock = threading.Lock()
-task_info = {'total': 0, 'processed': 0, 'time_start': None}
-
-
-def print_task():
-    time_used = dateutil.now() - task_info['time_start']
-    time_estimate = datetime.timedelta(
-        seconds=time_used.total_seconds() * (task_info['total'] / task_info['processed']))
-    logger.info("Process: %.2f%% [%s/%s], Estimate: [%s/%s]\r\n" % (
-        task_info['processed'] / task_info['total'] * 100, task_info['processed'], task_info['total'],
-        dateutil.date_to_str(time_used, dateutil.FORMAT_HMS),
-        dateutil.date_to_str(time_estimate, dateutil.FORMAT_HMS)))
-
-
-def func_wrapper(list_objs_: list, _func_) -> None:
-    _func_(list_objs_)
-    task_lock.acquire()
-    task_info['processed'] = task_info['processed'] + len(list_objs_)
-    task_lock.release()
-
-
-def thread_partition_call(list_obj: list, _func_, thread_num: int, partition_num: int) -> None:
-    list_partition = listutil.partition(list_obj, partition_num)
-    threads = []
-    logger.info("==================== start ====================")
-    task_info['total'] = len(list_obj)
-    task_info['time_start'] = dateutil.now()
-    for index_, list_for_process in enumerate(list_partition):
-        t = threading.Thread(target=func_wrapper, args=(list_for_process, _func_))
-        t.start()
-        threads.append(t)
-        if len(threads) == thread_num or index_ == len(list_partition) - 1:
-            for t_ in threads:
-                t_.join()
-            threads = []
-            logger.info("====================  end  ====================")
-            print_task()
-            if index_ < len(list_partition) - 1:
-                logger.info("==================== start ====================")
-
 
 def async_call(async_func, callback_func, *args, **kwargs):
     def wrapper():
@@ -62,3 +22,47 @@ def async_call(async_func, callback_func, *args, **kwargs):
 
     thread = threading.Thread(target=wrapper)
     thread.start()
+
+
+class PartitionExecutor:
+    def __init__(self):
+        task_lock = threading.Lock()
+        task_info = {'total': 0, 'processed': 0, 'time_start': None}
+
+    def print_task(self):
+        time_used = dateutil.now() - self.task_info['time_start']
+        time_estimate = datetime.timedelta(
+            seconds=time_used.total_seconds() * (self.task_info['total'] / self.task_info['processed']))
+        logger.info("Process: %.2f%% [%s/%s], Estimate: [%s/%s]\r\n" % (
+            self.task_info['processed'] / self.task_info['total'] * 100, self.task_info['processed'],
+            self.task_info['total'],
+            dateutil.date_to_str(time_used, dateutil.FORMAT_HMS),
+            dateutil.date_to_str(time_estimate, dateutil.FORMAT_HMS)))
+
+    def func_wrapper(self, list_objs_: list, _func_) -> None:
+        _func_(list_objs_)
+        self.task_lock.acquire()
+        self.task_info['processed'] = self.task_info['processed'] + len(list_objs_)
+        self.task_lock.release()
+
+    def thread_partition_call(self, list_obj: list, _func_, thread_num: int, partition_num: int) -> None:
+        list_partition = listutil.partition(list_obj, partition_num)
+        threads = []
+        logger.info("==================== start ====================")
+        self.task_info['total'] = len(list_obj)
+        self.task_info['time_start'] = dateutil.now()
+        for index_, list_for_process in enumerate(list_partition):
+            t = threading.Thread(target=func_wrapper, args=(list_for_process, _func_))
+            t.start()
+            threads.append(t)
+            if len(threads) == thread_num or index_ == len(list_partition) - 1:
+                for t_ in threads:
+                    t_.join()
+                threads = []
+                logger.info("====================  end  ====================")
+                self.print_task()
+                if index_ < len(list_partition) - 1:
+                    logger.info("==================== start ====================")
+
+
+partition_executor = PartitionExecutor()
