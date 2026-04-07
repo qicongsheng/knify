8dffcad643ee83ae8d305289043dcc47d656d3e5
qicongsheng <875881559@qq.com>
2026-04-08 00:26:20 +0800

    ignore

--- knify/help.py
+++ knify/help.py
@@ -6,7 +6,7 @@ def get_name():
 
 
 def get_version():
-    return '1.9.66'
+    return '1.9.68'
 
 
 def print_version():
--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ setup(
         'pytz',
         'requests',
         'curlify',
+        'tzdata',
         'xlrd'
     ]
 )
